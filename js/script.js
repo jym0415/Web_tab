30cc02fe9214aa0dafd208fd562470d90c534009
lbaku89 <khj930410@naver.com>
2022-02-28 01:49:15 +0900

    3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
+
+  $('ul.d1').mouseover(function(){
+    $('ul.d2').slideDown();
+  })
+
+  $('ul.d1').mouseleave(function(){
+    $('ul.d2').slideUp();
+  })
+
 })
