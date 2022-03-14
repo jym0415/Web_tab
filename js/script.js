711f88ba79c61b01c5e65248d8bc1ee46c3be020
jym0415 <jym0415@gamil.com>
2022-03-14 16:59:45 +0900

    슬라이드

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+
 $(document).ready(function(){
 
-  $('ul.d1').mouseover(function(){
-    $('ul.d2').slideDown();
-  })
+  var px = $("#slider").height() //1200px 위로갈땐 height 좌우 width
+  var dir = -1; //방향 - 오른쪽방향일땐 1
 
-  $('ul.d1').mouseleave(function(){
-    $('ul.d2').slideUp();
-  })
+  //json 포멧에서는 key는 절대 변수로 사용불가
+
+  // $('#slider ul').animate({ "marginLeft" : px*dir }, 400, function(){
+  // 먼저 검수하기!
+
+  setInterval(function(){
+    $('#slider ul').animate({ "marginTop" : px*dir }, 400, function(){
+      $(this).find("li").eq(0).appendTo($(this));
+      $(this).css("margin-Top",0); //위로올라갈땐 margin-Top 좌우margin-left
+    });
+  },3000);
 
 })
